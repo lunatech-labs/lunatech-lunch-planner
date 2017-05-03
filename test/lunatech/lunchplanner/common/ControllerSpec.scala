f3953769536e1688af9f686c49bf836d50af5bb9
Leonor Boga <leonor.boga@lunatech.com>
2017-05-03 17:27:00 +0200

    Added DishControllerSpec. Added test to the list of dishes

---
+++ test/lunatech/lunchplanner/common/ControllerSpec.scala
@@ -0,0 +1,21 @@
+package lunatech.lunchplanner.common
+
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.mockito.MockitoSugar
+import org.scalatestplus.play.PlaySpec
+import org.scalatestplus.play.guice.GuiceOneAppPerSuite
+import play.api.mvc.Results
+import play.api.test.Writeables
+
+import scala.concurrent.duration._
+
+trait ControllerSpec
+  extends PlaySpec
+  with Results
+  with MockitoSugar
+  with BeforeAndAfterAll
+  with GuiceOneAppPerSuite
+  with Writeables {
+
+  val defaultTimeout = 10.seconds
+}
