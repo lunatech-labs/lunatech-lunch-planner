0bfc3b066f47909cbbfd893b6c267b874d2b0386
Sandeep Purohit <sandeep@Sandeeps-MacBook-Pro.local>
2017-05-22 17:29:00 +0200

    replace Calender api with nscala-time

--- app/lunatech/lunchplanner/utils/MailerUtils.scala
+++ app/lunatech/lunchplanner/utils/MailerUtils.scala
@@ -8,7 +8,7 @@ import lunatech.lunchplanner.common.DBConnection
 import lunatech.lunchplanner.persistence.UserTable
 import play.api.Configuration
 import play.api.libs.mailer.{Email, MailerClient}
-
+import com.github.nscala_time.time.Imports._
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.Duration
 
@@ -16,26 +16,19 @@ import scala.concurrent.duration.Duration
   * This is utility to send mail to all the users on every monday
   *
   * @param mailerClient is used for send mail
-  * @param connection is the used for connection to db for retrieve all users
+  * @param connection   is the used for connection to db for retrieve all users
   *
   *
   */
-class MailerUtils @Inject()(mailerClient: MailerClient,configuration: Configuration, implicit val connection: DBConnection) {
-
-  import java.util.Calendar
+class MailerUtils @Inject()(mailerClient: MailerClient, configuration: Configuration, implicit val connection: DBConnection) {
 
   private def timeToNextMonday = {
-    val now = Calendar.getInstance
-    now.set(Calendar.HOUR, 23)
-    now.set(Calendar.MINUTE, 50)
-    val weekday = now.get(Calendar.DAY_OF_WEEK)
-    if (weekday != Calendar.MONDAY) { // calculate how much to add
-      // the 2 is the difference between Saturday and Monday
-      val days = (Calendar.SATURDAY - weekday + 2) % 7
-      now.add(Calendar.DAY_OF_YEAR, days)
-    }
-    val date = now.getTime
-    (now.getTime.getTime - System.currentTimeMillis) / (1000 * 60)
+
+    val today = LocalDate.now
+    val dayOfWeek = today.getDayOfWeek
+    val nextMonday = today.plusDays(8 - dayOfWeek)
+    val nextMondayTime = nextMonday.toDateTime(new LocalTime(15, 30))
+    (nextMondayTime.getMillis - System.currentTimeMillis) / (1000 * 60)
   }
 
   def send(sendTo: List[String], body: String): String = {
--- build.sbt
+++ build.sbt
@@ -25,6 +25,7 @@ libraryDependencies ++= Seq(
   "com.enragedginger" %% "akka-quartz-scheduler" % "1.6.0-akka-2.4.x",
   "org.scalaz" %% "scalaz-core" % "7.2.10",
   "com.typesafe.play" %% "play-mailer" % "5.0.0",
+  "com.github.nscala-time" %% "nscala-time" % "2.16.0",
   "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.0" % Test,
   "org.mockito" % "mockito-all" % "1.10.19" % Test
 )
