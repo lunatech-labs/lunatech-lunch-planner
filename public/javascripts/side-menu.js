9dbabc19bb0917d14f7c2be85b045dc5b6215af8
Sandeep Purohit <sandeep@Sandeeps-MacBook-Pro.local>
2017-05-26 13:46:35 +0200

    refactor side-menu js

--- public/javascripts/side-menu.js
+++ public/javascripts/side-menu.js
@@ -1,16 +1,15 @@
 $(function() {
-$(document).ready(function(){
-var selected=window.location.pathname;
-console.log(selected);
-if(selected=="/")
-$("#planned").addClass('selected');
-else if(selected=="/dish/all")
-$("#dishes").addClass('selected');
-else if(selected=="/menu/all")
-$("#menus").addClass('selected');
-else if(selected=="/menuPerDay/all")
-$("#schedule").addClass('selected');
-else if(selected=="/profile")
-$("#profile").addClass('selected');
-});
+    $(document).ready(function(){
+        var selected=window.location.pathname;
+        if(selected=="/")
+        $("#planned").addClass('selected');
+        else if(selected=="/dish/all")
+        $("#dishes").addClass('selected');
+        else if(selected=="/menu/all")
+        $("#menus").addClass('selected');
+        else if(selected=="/menuPerDay/all")
+        $("#schedule").addClass('selected');
+        else if(selected=="/profile")
+        $("#profile").addClass('selected');
+    });
 });
