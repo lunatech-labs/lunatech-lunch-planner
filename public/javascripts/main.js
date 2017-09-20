0250112aa3eac1f4cdb4c3f27f5be39a37c14347
arniel <arniel.agomaa@lunatech.com>
2017-09-20 13:13:17 +0200

    Added front-end validation for selecting a meal schedule.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,17 +1,57 @@
 $(function(){
+    'use strict';
+
+    /**
+     * Will perform "smart" select all. Will only check one item per day.
+     * 1. If there's already a check for that day, it will remain unchanged.
+     * 2. If there's only one meal for that day, it will be checked regardless of location.
+     * 3. It will always go for the default location, which is Rotterdam. Unless we implement a "preferred location" per user.
+     * 4. If there's more than one meal with the same location, it will check the first meal in the list.
+     */
+    function smartSelectAll(defaultLocation) {
+        var dates = $("input[data-date]").map(function() {
+            return $(this).attr("data-date");
+        }).get();
+        var uniqueDates = jQuery.unique(dates);
+
+        uniqueDates.forEach(function(date) {
+            var hasCheck = false;
+            var inputsByDate = $("input[data-date='" + date + "']");
+
+            var checkedInputsByDate = $("input[data-date='" + date + "']:checked");
+            if(checkedInputsByDate.length == 0) {
+                inputsByDate.each(function() {
+                    if(inputsByDate.length == 1) {
+                        this.checked = true;
+                    } else {
+                        var location = $(this).parents(".schedule-row").children(".js-menu-location").attr("value");
+                        if (!hasCheck && defaultLocation == location) {
+                            this.checked = true;
+                            hasCheck = true;
+                        }
+                    }
+                })
+            }
+        });
+    }
+
     $('#select-all').click(function(event) {
-        if(this.checked) {
-            // Iterate each checkbox
-            $('.schedule:checkbox').each(function() {
-                this.checked = true;
-            });
-        }
-        else {
-            $(':checkbox').each(function() {
+        if (this.checked) {
+            smartSelectAll("Rotterdam");
+        } else {
+            $(':checkbox').each(function () {
                 this.checked = false;
             });
         }
     });
+});
+
+$(function allSelected(){
+    var allSelected = $('.schedule:checked').length === $('.schedule').length;
+
+    if(allSelected) {
+        $('input#select-all').prop("checked", true);
+    }
 });
 
 $(function(){
@@ -33,29 +73,29 @@ $(function(defaultDateStart, defaultDateEnd){
     });
 });
 
-$(document).ready(function() {
-    allSelected();
+$(function verifySelectedMeals() {
+    $(".schedule").change(function () {
+        if (this.checked) {
+            var date = $(this).attr("data-date");
+            var checkboxes = $("input[data-date='" + date + "']:checked");
+            if (checkboxes.length >= 2) {
+                this.checked = false;
+                $.toaster({
+                    priority: 'danger',
+                    title: 'Ah ah ah!',
+                    message: 'You can only attend one meal per day.'
+                });
+            }
+        }
+    });
 });
-
-$(".schedule").change(function(){
-    allSelected();
-});
-
-function allSelected(){
-    var allSelected = $('.schedule:checked').length === $('.schedule').length;
-
-    if(allSelected) {
-        $('#select-all').each(function() {
-            this.checked = true;
-        });
-    }
-}
 
 $(function() {
     $('#formAttendance').submit(function (eventObj) {
         $('.schedule:checked').each(function () {
-            $(this).append('<input type="hidden" name="menuDate[]" value="' + this.parentNode.parentNode.cells.item(1).textContent.toString() + '" />');
+            $(this).append('<input type="hidden" name="menuDate[]" value="' + $(this).parents(".schedule-row").children(".js-menu-date").attr("value") + '" />');
         });
         return true;
     });
 });
+
