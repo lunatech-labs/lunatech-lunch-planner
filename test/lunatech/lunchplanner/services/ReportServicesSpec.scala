9b6627f8416d1679c71aa9fdffd0a599c1718dae
arniel <arniel.agomaa@lunatech.com>
2017-10-13 14:16:40 +0200

    Unit test fix for report

--- test/lunatech/lunchplanner/services/ReportServicesSpec.scala
+++ test/lunatech/lunchplanner/services/ReportServicesSpec.scala
@@ -5,32 +5,33 @@ import java.util.UUID
 
 import lunatech.lunchplanner.common.DBConnection
 import lunatech.lunchplanner.models.{MenuPerDay, Report}
-import org.scalatest.mockito.MockitoSugar
-import org.scalatestplus.play.PlaySpec
 import org.mockito.Matchers.any
 import org.mockito.Mockito._
+import org.scalatest.mockito.MockitoSugar
+import org.scalatestplus.play.PlaySpec
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.{Await, Future}
 import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
 
 class ReportServicesSpec extends PlaySpec with MockitoSugar {
 
   val menuPerDayPerPersonService = mock[MenuPerDayPerPersonService]
   val menuPerDayService = mock[MenuPerDayService]
-  val connection = mock[DBConnection]
+  implicit val connection = mock[DBConnection]
   val randomUUID = UUID.randomUUID()
   val defaultTimeout = 10.seconds
 
   when(menuPerDayService.getAllOrderedByDateFilterDateRange(any[Date], any[Date]))
-    .thenReturn(Future(Seq(MenuPerDay(randomUUID, randomUUID, any[Date]))))
+    .thenReturn(Future(Seq(MenuPerDay(randomUUID, randomUUID, any[Date], "Rotterdam"))))
   when(menuPerDayPerPersonService.getListOfPeopleByMenuPerDay(randomUUID)).thenReturn(Future(Nil))
+  when(menuPerDayPerPersonService.getListOfPeopleByMenuPerDayForReport(any[MenuPerDay])).thenReturn(Future(Nil))
 
   val reportService = new ReportService(menuPerDayPerPersonService, menuPerDayService, connection)
 
   "report service" should {
     "report service generate report for controller" in {
-      val report=Await.result(reportService.getReport(any[Date]),defaultTimeout)
+      val report=Await.result(reportService.getReport(1), defaultTimeout)
       report mustBe Report(Map.empty)
     }
   }
